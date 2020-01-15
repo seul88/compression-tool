5566204fbc17d55ec1ac2154c2541bf22988a013
Erwin Majewski <erwin.majewski@man.poznan.pl>
2020-01-15 21:48:15 +0100

    Add file dropzone

--- frontend/src/components/LoadFile.jsx
+++ frontend/src/components/LoadFile.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, {useMemo} from 'react';
+import {useDropzone} from 'react-dropzone';
 
-export default function LoadFile() {
-    return (
-        <div className="mt-3">
-            Drag and drop component here
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out'
+};
+
+const activeStyle = {
+  borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
+
+
+export default function LoadFile(props) {
+    const {
+        getRootProps,
+        getInputProps,
+        isDragActive,
+        isDragAccept,
+        isDragReject,
+        acceptedFiles
+      } = useDropzone();
+    
+      const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isDragActive ? activeStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+      }), [
+        isDragActive,
+        isDragReject
+      ]);
+    
+      console.log(acceptedFiles)
+
+        const files = acceptedFiles.map(file => (
+            <li key={file.path}>
+            {file.path} - {file.size} bytes
+            </li>
+        ));
+
+      return (
+        <div className="container">
+          <div {...getRootProps({style})}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+            {files}
+          </div>
         </div>
-    )
-}
+      );
+    }
