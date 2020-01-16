9731911868538ccf74925256a294a8150e9188b0
Erwin Majewski <erwin.majewski@man.poznan.pl>
2020-01-16 21:32:22 +0100

    Add django-cors-headers package to virtual environment

--- backend/backendApp/backendApp/settings.py
+++ backend/backendApp/backendApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	 'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backendApp.urls'
