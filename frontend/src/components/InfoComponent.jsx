c60f60695742ebc26fea9b6f501c32970cbd1e61
Erwin Majewski <erwin.majewski@man.poznan.pl>
2020-01-17 21:22:08 +0100

    Add info component

--- frontend/src/components/InfoComponent.jsx
+++ frontend/src/components/InfoComponent.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function InfoComponent() {
     return (
-        <div className="mt-1">
-            Info / header component
+        <div className="mt-4 mb-3">
+            <h3><b>Benchmark kompresji plików</b></h3>
+            <div >Wczytaj plik znajdujący się na dysku i dobierz parametry kompresji. </div>
+            <div >Narzędzie przeanalizuje, jaka metoda kompresji jest optymalna pod względem czasu obliczeń i stopnia kompresji.</div>
         </div>
     )
 }
